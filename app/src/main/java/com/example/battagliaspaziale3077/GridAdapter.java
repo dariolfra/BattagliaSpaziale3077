52669f8cef0213ff6425605097cf5f59914c9889
Perin <perlnmarck@gmail.com>
2024-05-23 14:59:31 +0200

    Le navi ruotano, non escono dalla tabella e non si sovrappongono

--- app/src/main/java/com/example/battagliaspaziale3077/GridAdapter.java
+++ app/src/main/java/com/example/battagliaspaziale3077/GridAdapter.java
@@ -51,15 +51,116 @@ public class GridAdapter extends BaseAdapter {
         return convertView;
     }
 
-    public boolean ControllaSeLiberi(int position, int size) {
+    public boolean ControllaSeLiberi(int position, int size,int index, int rotation) {
+        //Per risolvere bug
         boolean celleLibere = true;
-        for (int i = 0; i < size; i++){
-            if(immaginiCasella[position + i] != 0){
+        if(index == 0 && (rotation == 0 || rotation == 180 || rotation == 90) ||
+                index == 1 && (rotation == 0 || rotation == 180) ||
+                index == 2 && (rotation == 0 || rotation == 90) ||
+                index == 3 && (rotation == 0 || rotation == 180) ||
+                index == 4 && rotation == 90 ||
+                index == 5 && rotation == 90) {
+            int[] invalidPositions = {9, 19, 29, 39, 49, 59, 69, 79, 89, 99};
+            for (int invalidPosition : invalidPositions) {
+                if (position == invalidPosition) {
+                    return false;
+                }
+            }
+        }
+
+        //Verifica se le celle dove la nave deve essere inserita sono libere
+        for (int i = 0; i < size; i++) {
+            int newPosition = position + i;
+            if (newPosition < 0 || newPosition >= immaginiCasella.length || immaginiCasella[newPosition] != 0) {
                 return false;
+            }
+            if ((index == 0 || index == 1 || index == 2 || index == 3 || index == 4) && rotation == 0 || (index == 1 || index == 3) && rotation == 180 || index == 2 && rotation == 270)  {
+                if (CellaOccupata(position + i, immaginiCasella)) {
+                    return false;
+                }
+                if (index == 0 && i == 1 || index == 2 && i == 0 && rotation == 0 || index == 2 && i == 1 && rotation == 270 || index == 4 && i == 2) {
+                    if (CellaOccupata(position + i - 10, immaginiCasella)) {
+                        return false;
+                    }
+                }
+            } else if ((index == 0 || index == 1 || index == 3 || index == 4) && rotation == 90 || (index == 1 || index == 3) && rotation == 270) {
+                if (CellaOccupata(position + i * 10, immaginiCasella)) {
+                    return false;
+                }
+                if (index == 0 && i == 1 || index == 4 && i == 2) {
+                    if (CellaOccupata(position + 1 + i * 10, immaginiCasella)) {
+                        return false;
+                    }
+                }
+            } else if ((index == 0 || index == 4) && rotation == 180 || index == 2 && (rotation == 90 || rotation == 180)) {
+                if (CellaOccupata(position + i, immaginiCasella)) {
+                    return false;
+                }
+                if (index == 0 && i == 1 || index == 2 && i == 0 && rotation == 90 || index == 2 && i == 1 && rotation == 180 || index == 4 && i == 2) {
+                    if (CellaOccupata(position + i + 10, immaginiCasella)) {
+                        return false;
+                    }
+                }
+            } else if ((index == 0 || index == 4) && rotation == 270) {
+                if (CellaOccupata(position + i * 10, immaginiCasella)) {
+                    return false;
+                }
+                if (index == 0 && i == 1 || index == 4 && i == 2) {
+                    if (CellaOccupata(position - 1 + i * 10, immaginiCasella)) {
+                        return false;
+                    }
+                }
+
             }
         }
         return celleLibere;
     }
+    public int AggiustaPosizioni(int index,int rotation,int position){
+        //Aggiustamenti di posizioni altrimenti l'immagine non verrà posizionata dove l'utente ha messo la nave
+        int posizione = position;
+        if ((index == 0 || index == 1 || index == 2) && rotation == 90){
+            if(position % 10 == 0){
+                posizione = position - 10;
+            }
+            else {
+                posizione = position - 9;
+            }
+        } else if (index == 3 && rotation == 90) {
+            if(position % 10 == 0 || position % 9 == 0){
+                posizione = position - 20;
+            }
+            else {
+                posizione = position - 19;
+            }
+        } else if(index == 3 && (rotation == 0 || rotation == 180)){
+            posizione = position - 1;
+        } else if (index == 4 && rotation == 90) {
+            posizione = position - 20 ;
+        } else if (index == 5 && rotation == 90|| index == 5 && rotation == 180 && position < 99|| index == 0 && rotation == 180 && position < 99){ //altrimenti la nave la mette una riga sotto
+            posizione = position - 10;
+        } else if ((index == 5 || index == 0 ) && rotation == 270){
+            posizione = position - 9;
+        } else if (index == 4 && rotation == 180) {
+            posizione = position - 11;
+        } else if (index == 4 && rotation == 270) {
+            posizione = position - 19;
+        }
+        return posizione;
+    }
+    public int getColumnFromPosition(int position) {
+        return position % 10;
+    } //colonne da 0 a 9
+    public boolean CellaOccupata(int posizione,int[] immaginiCasella){
+        if(immaginiCasella[posizione] != 0){
+            return true; //se è occupata
+        }
+        else {
+            return false;
+        }
+    }
 }
+
+
+
 
 
--- app/src/main/java/com/example/battagliaspaziale3077/MainActivity.java
+++ app/src/main/java/com/example/battagliaspaziale3077/MainActivity.java
@@ -4,26 +4,30 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.battagliaspaziale3077.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     //Effettura controllo della modalita che viene passata dalla pagina che crea il gioco
+
+    //Variabili per il gioco
     ActivityMainBinding binding;
     ImageView[] navi;
     float startX, startY;
+    float[] initialX, initialY;
     int[] shipSizes = {3, 2, 2, 4,4,3}; // Dimensioni delle navi
+    int[] rotationDegrees = {0, 0, 0, 0, 0, 0}; // Gradi di rotazione delle navi
+
+
     Boolean dati_arrivati_correttamente = false;
     Context context;
     String nome_giocatore;
@@ -47,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
         navi[4] = findViewById(R.id.navedaottorino);
         navi[5] = findViewById(R.id.navel);
 
-
+        // Inizializza gli array per le posizioni iniziali
+        initialX = new float[navi.length];
+        initialY = new float[navi.length];
 
         context = this.getApplicationContext();
 
@@ -69,60 +75,177 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.gridView.setAdapter(gridAdapter);
+        // Utilizza un ViewTreeObserver per memorizzare le posizioni iniziali dopo il layout
+        ViewTreeObserver viewTreeObserver = binding.getRoot().getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Memorizza le posizioni iniziali delle navi
+                for (int i = 0; i < navi.length; i++) {
+                    initialX[i] = navi[i].getX();
+                    initialY[i] = navi[i].getY();
+                }
+                // Rimuovi il listener per evitare che venga chiamato più volte
+                binding.getRoot().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+
         for (int i = 0; i < navi.length; i++) {
             final int index = i;
+
+            GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapConfirmed(MotionEvent e) {
+                    // Ruota l'immagine di 90 gradi
+                    rotationDegrees[index] = (rotationDegrees[index] + 90) % 360;
+                    navi[index].setRotation(rotationDegrees[index]);
+                    return true;
+                }
+            });
             navi[i].setOnTouchListener((v, event) -> {
+                gestureDetector.onTouchEvent(event);
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        startX = event.getX();
-                        startY = event.getY();
+                        startX = event.getRawX();
+                        startY = event.getRawY();
                         break;
                     case MotionEvent.ACTION_MOVE:
-                        float dx = event.getX() - startX;
-                        float dy = event.getY() - startY;
+                        float dx = event.getRawX() - startX;
+                        float dy = event.getRawY() - startY;
+
                         navi[index].setX(navi[index].getX() + dx);
                         navi[index].setY(navi[index].getY() + dy);
+
+                        startX = event.getRawX();
+                        startY = event.getRawY();
                         break;
                     case MotionEvent.ACTION_UP:
                         int[] gridLocation = new int[2];
                         binding.gridView.getLocationInWindow(gridLocation);
-                        float x = navi[index].getX() + navi[index].getWidth() / 2;
-                        float y = navi[index].getY() + navi[index].getHeight() / 2;
+                        float x = navi[index].getX() + (float) navi[index].getWidth() / 2;
+                        float y = navi[index].getY() + (float) navi[index].getHeight() / 2;
                         int column = (int) ((x - gridLocation[0]) / (binding.gridView.getWidth() / 10) - 1);
                         int row = (int) ((y - gridLocation[1]) / (binding.gridView.getHeight() / 10) + 1);
                         int position = row * 10 + column;
                         int size = shipSizes[index];
-                        if (column + size <= 10 && gridAdapter.ControllaSeLiberi(position, size)) {
-                            for (int j = 0; j < size; j++) {
-                                if(position < 10 && index == 0 || position < 10 && index == 2 || position < 10 && index == 4 || position < 10 && index == 5){
-                                    CustomToast.showToast(this, "La nave non può essere posizionata qui " + position, Toast.LENGTH_SHORT);
-                                    break;
-                                }
-                                else{
-                                    immaginiCasella[position + j] = R.drawable.naveda1;
-                                    if(index == 0 && j == 1 || index == 2 && j == 0 || index == 4 && j == 2 || index == 5 && j == 2){
-                                        int p = position - 10;
-                                        immaginiCasella[p + j] = R.drawable.naveda1;
-                                    }
-                                    navi[index].setVisibility(View.INVISIBLE);
-                                }
-
-                            }
-                            //Toast.makeText(this, "Posizione della nave " + position + " - " + (position + size - 1), Toast.LENGTH_SHORT).show();
-
-                        } else {
-
-                            CustomToast.showToast(this, "La nave non può essere posizionata qui " + position, Toast.LENGTH_SHORT);
+                        int posizione = gridAdapter.AggiustaPosizioni(index,rotationDegrees[index],position); //per sistemare le posizioni
+                        if (ControllaSeOutBound(gridAdapter.getColumnFromPosition(posizione),size,index,rotationDegrees[index],posizione) && gridAdapter.ControllaSeLiberi(posizione, size,index,rotationDegrees[index])) {
+                            //inserimento delle navi
+                            posizionaNave(index,size,rotationDegrees[index],posizione,immaginiCasella);
                         }
-
-                        navi[index].setX(startX); // Resetta la posizione x
-                        navi[index].setY(startY);  // Resetta la posizione y
+                        // Resetta la posizione x e y alle posizioni iniziali
+                        navi[index].setX(initialX[index]);
+                        navi[index].setY(initialY[index]);
                         gridAdapter.notifyDataSetChanged();
                         break;
                 }
                 return true;
             });
         }
-        binding.gridView.setAdapter(gridAdapter);
     }
+    public boolean ControllaSeOutBound(int column, int size, int index, int rotation, int position) {
+
+        // Controllo speciale per la nave 4
+        if (index == 3 && (rotation == 0 || rotation == 180) && column + size == 11) {
+            return true;
+        }
+
+        // Controllo delle posizioni fuori dai limiti per rotazioni specifiche
+        if (index == 0 && rotation == 0 && position < 10 ||
+                (index == 0 && rotation == 270 && (column <= 0 || position > 79)) ||
+                (index == 0 && rotation == 90 && position > 79) ||
+                (index == 1 && (rotation == 90 || rotation == 180) && position > 89) ||
+                index == 2 && rotation == 0 && position < 10 ||
+                (index == 2 && ((rotation == 90 && (position - 8) % 10 == 0) ||
+                        (rotation == 180 && position > 89) ||
+                        (rotation == 270 && position < 10))) ||
+                (index == 3 && (rotation == 90 || rotation == 270) && (position < 9 || position > 69)) ||
+                (index == 4 && rotation == 0 && position < 10) ||
+                (index == 4 && rotation == 90 && position > 69) ||
+                (index == 4 && rotation == 270 && column == 0) ||
+                (index == 5 && rotation == 0 && position < 10) ||
+                (index == 5 && rotation == 270 && column == 0) ||
+                (index == 5 && ((rotation == 90 || rotation == 270) && position > 79)))
+        {
+            return false;
+        }
+
+        // Controlli per rotazioni specifiche e colonne
+        if ((index == 3 && (rotation == 90 || rotation == 270) && position >= 9) ||
+                (index == 1 && (rotation == 90 || rotation == 270)) && position < 90 ||
+                (index == 4 && ((rotation == 90 && position > 19 && position <= 89) ||
+                        (rotation == 180 && position < 90 && column < 7) ||
+                        (rotation == 270 && column > 0 && position < 69)))  ||
+                (index == 5 && ((rotation == 90 || rotation == 270) && position < 79))) {
+            return true;
+        }
+        // Controllo generale delle colonne
+        return column + size <= 10;
+    }
+    private void posizionaNave(int index, int size, int rotationDegrees, int posizione,int[] immaginiCasella) {
+        //Posizionamento della nava cella per cella
+        for (int j = 0; j < size; j++) {
+            int currentPos = posizione + j;
+            int p;
+
+            switch (index) {
+                case 0:
+                case 1:
+                case 2:
+                case 4:
+                case 5:
+                    if (rotationDegrees == 0 || rotationDegrees == 180) {
+                        immaginiCasella[currentPos] = R.drawable.naveda1;
+                        if ((index == 2 && j == 0 && rotationDegrees == 0) || (index == 0 && j == 1 && rotationDegrees == 0) || (index == 4 && j == 2 && rotationDegrees == 0) || (index == 5 && j == 2 && rotationDegrees == 0)) {
+                            p = posizione - 10 + j;
+                            immaginiCasella[p] = R.drawable.naveda1;
+                        }
+                    } else if (rotationDegrees == 90 || rotationDegrees == 270) {
+                        if(index == 2 && rotationDegrees == 270){
+                            immaginiCasella[posizione + j] = R.drawable.naveda1;
+                        }
+                        else {
+                            immaginiCasella[posizione + j * 10] = R.drawable.naveda1;
+                        }
+                        if ((index == 0 && j == 1 && rotationDegrees == 90) || (index == 2 && j == 0 && rotationDegrees == 90) || (index == 5 && j == 2 && rotationDegrees == 90) || (index == 4 && j == 2 && rotationDegrees == 90)) {
+                            p = posizione + j * 10 + 1;
+                            immaginiCasella[p] = R.drawable.naveda1;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    if (rotationDegrees == 0 || rotationDegrees == 180) {
+                        immaginiCasella[currentPos] = R.drawable.naveda1;
+                    } else if (rotationDegrees == 90 || rotationDegrees == 270) {
+                        immaginiCasella[posizione + j * 10] = R.drawable.naveda1;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Additional checks for specific rotations
+            if (rotationDegrees == 180) {
+                if ((index == 2 && j == 1) || (index == 0 && j == 1) || (index == 5 && j == 0) || (index == 4 && j == 1)) {
+                    p = posizione + 10 + j;
+                    immaginiCasella[p] = R.drawable.naveda1;
+                }
+            } else if (rotationDegrees == 270) {
+                if ((index == 4 && j == 1) || (index == 0 && j == 1) || (index == 5 && j == 0)) {
+                    p = posizione + j * 10 - 1;
+                    immaginiCasella[p] = R.drawable.naveda1;
+                } else if (index == 2 && j == 1) {
+                    p = posizione - 10 + 1;
+                    immaginiCasella[p] = R.drawable.naveda1;
+                }
+            }
+        }
+        navi[index].setVisibility(View.INVISIBLE);
+    }
+
+
 }
