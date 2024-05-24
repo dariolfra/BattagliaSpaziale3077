b02885626b5bd652409236b08c033614806a8263
Perin <perlnmarck@gmail.com>
2024-05-24 11:10:08 +0200

    image mossa speciale

--- app/src/main/java/com/example/battagliaspaziale3077/MainActivity.java
+++ app/src/main/java/com/example/battagliaspaziale3077/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         btnConferma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
