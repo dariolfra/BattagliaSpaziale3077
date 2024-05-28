47eed644b701eb9bf6d2409ecc39f7abf73d9e0e
RPette <118899569+RPette@users.noreply.github.com>
2024-05-28 12:38:42 +0200

    grafica difesa

--- app/src/main/java/com/example/battagliaspaziale3077/Defence.java
+++ app/src/main/java/com/example/battagliaspaziale3077/Defence.java
@@ -8,6 +8,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
 
 import java.util.HashMap;
 
@@ -24,6 +25,7 @@ public class Defence extends Game {
     private boolean multiplayer;
     private ImageView background ,immagine_pers;
     private TextView giocatore1,giocatore2;
+    private Button btn_vai_a_attacco;
     private Context context;
     private int[] casellaColpita;
 
@@ -43,6 +45,7 @@ public class Defence extends Game {
         giocatore2 = (TextView) findViewById(R.id.txtNomeG2);
         immagine_pers = (ImageView) findViewById(R.id.img_pers);
         background = (ImageView) findViewById(R.id.background);
+        btn_vai_a_attacco = (Button) findViewById(R.id.btn_vai_a_attacco);
 
         context = this.getApplicationContext();
 
