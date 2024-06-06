5cf2612e86ddb11b23139c798a4a3632f881b96e
RPette <118899569+RPette@users.noreply.github.com>
2024-06-06 10:59:16 +0200

    comments

--- app/src/main/java/com/example/battagliaspaziale3077/Attack.java
+++ app/src/main/java/com/example/battagliaspaziale3077/Attack.java
@@ -203,6 +203,7 @@ public class Attack extends Game implements Serializable {
                         defence.putExtra("NaviAffondate", (Serializable) NaviAffondate);
                         defence.putExtra("comms", comms);
                         startActivity(defence);
+                        //ciao mondo
                     }
 
                 } catch (InterruptedException e) {
@@ -325,7 +326,7 @@ public class Attack extends Game implements Serializable {
         });
     }
 
-    public void confermaAttacco(View v) {
+    /*public void confermaAttacco(View v) {
         try {
             btn_attacca.startAnimation(scale_down);
             btn_attacca.startAnimation(scale_up);
@@ -360,7 +361,7 @@ public class Attack extends Game implements Serializable {
         } catch (Exception e) {
             CustomToast.showToast(context, "SELEZIONA UNA CASELLA!", delay);
         }
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
